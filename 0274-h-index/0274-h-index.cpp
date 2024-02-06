db3764f9ebcab7ec61c6fff4c6b5bd3302423af7
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-02-06 14:29:41 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (5.12%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,29 +1,6 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        /*
-            We will use a map to map out the values to their occurences and then have a running counter
-            for eg. [[3, 0, 6, 1, 5]]
-            ->
-            {
-                3:1
-                0:1
-                6:1
-                1:1
-                5:1
-            }
-            sort this map or order this map. If we use an ordered map-> we will have complexity->o(nlongn)
-            after sorting->
-            {
-                0:1
-                1:1
-                3:1
-                5:1
-                6:1
-            }
-            we iterate through this map and increase the counter in every iteration. 
-            At any point where key>=counter we set h index as that value
-        */
         map<int, int> citationMap;
         for(int i=0;i<citations.size();i++){
             if(citationMap.find(citations[i])==citationMap.end()){
@@ -43,10 +20,6 @@ public:
         int counter = 0;
         int minimumCitations = 0;
         for(auto it=citationMap.rbegin();it!=citationMap.rend();it++){
-            // cout<<it->first;
-            // cout<<it->second;
-            // cout<<counter;
-            // cout<<'\n';
             counter+=it->second;
             if(it->first<=counter and hIndex<it->first){
                 hIndex=it->first;
