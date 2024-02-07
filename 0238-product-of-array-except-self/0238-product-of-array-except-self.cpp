247ba1bcf9d381f0ba9e17fc3da18bbee7b9dee0
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-02-07 23:18:54 +0530

    Time: 80 ms (5.09%), Space: 27.5 MB (6.40%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        //two pointer -> double iteration question. Where we are going to iterate from the front and the back. 
+        // []
+        vector<int> leftValues(nums.size());
+        vector<int> rightValues(nums.size()); 
+        vector<int> solution(nums.size()); 
+        int leftMultiplicant = 1;
+        int rightMultiplicant = 1;
+        leftValues[0]=1;
+        rightValues[nums.size()-1]=1;
+        for(int i = 1;i<nums.size();i++){
+            leftMultiplicant*=nums[i-1];
+            leftValues[i]=(leftMultiplicant);
+        }
+        for(int i = nums.size()-2;i>=0;i--){
+            rightMultiplicant*=nums[i+1];
+            rightValues[i]=(rightMultiplicant);
+        }
+        for(int i=0;i<nums.size();i++){
+            cout<<leftValues[i];
+            cout<<"\t";
+            cout<<rightValues[i];
+            cout<<"\n";
+        }
+        for(int i = 0 ; i<nums.size();i++){
+            solution[i]=leftValues[i]*rightValues[i];
+        }
+        return solution;
+    }
+};
