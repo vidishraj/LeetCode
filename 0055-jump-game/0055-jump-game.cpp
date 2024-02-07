5a66b01d548e3f2ba540605f0bbfb1ac997fda4c
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-02-07 10:11:01 +0530

    Time: 51 ms (48.46%), Space: 50.9 MB (25.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,44 +1,10 @@
 class Solution {
 public:
-    int canJump(vector<int>& nums) {
-       vector<int> solution(nums.size());
-       int runningCounter=0;
-        if(nums.size()==1){     
-            return true;
-        }
-       solution[nums.size()-1]= 0;
-       runningCounter = 2; 
-       if(nums.size()==2){
-           if(nums[0]>=1){
-               return true;
-           }
-            return false;
-       }
-       if(nums[nums.size()-2]==0){
-        solution[nums.size()-2]=0;
-       }
-       else{
-        solution[nums.size()-2]=1;
-       }
-       for(int it = nums.size()-3;it>=0;it--){
-            if(nums[it]==0){
-                solution[it]=0;
-            }
-            if(nums[it]>=runningCounter){
-                solution[it]= 1;
-            }
-            else{
-               int min = 50000;
-               for(int j = it+1;j<=it+nums[it];j++){
-                if(solution[j]>0 and solution[j]<min){
-                    min=solution[j];
-                }
-               }
-               solution[it]=min==50000?0:min+1;
-            }
-           runningCounter++;
-       }
-        
-        return solution[0]>0?true:false;
+    bool canJump(vector<int>& nums) {
+    int i = 0;
+    int n = nums.size();
+    for (int reach = 0; i < n && i <= reach; ++i)
+        reach = max(i + nums[i], reach);
+    return i == n;
     }
 };
