ab01375e277b151ab1f60ca584d487d07a4e3684
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-02-12 19:44:48 +0530

    Time: 11 ms (48.00%), Space: 18.1 MB (7.45%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        /*
-            Key points to note are that we need to travel till the last index where the number is greater than or == to the target
-            then we use two pointers to find the solution. 
-            reducing the pointer from the right reduces the sum and increasing the right pointer increases the sum. Balance till target it found.
-        */
        int firstIndex=0;
        int lastIndex = -1;
        lastIndex= numbers.size()-1;
