f4e48281cd11a89153d17a404003690d13cbd995
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-02-18 15:04:46 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (6.50%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        if(nums.size()==0){
+            return result;
+        }
+        string start = to_string(nums[0]);
+        for(int i=0;i<nums.size()-1;i++){
+            long num1=long(nums[i+1]);
+            long num2 = long(nums[i]);
+            long long dif= num1-num2;
+            if(dif!=1){
+                string end = to_string(nums[i]);
+                if(stoi(start)==nums[i]){
+                    result.push_back(start);
+                }
+                else{
+                    result.push_back(start+"->"+end);
+                }
+                start = to_string(nums[i+1]);   
+            }
+        }
+        if(stoi(start)==nums[nums.size()-1]){
+            result.push_back(start);
+        }
+        else{
+                string end = to_string(nums[nums.size()-1]);
+                result.push_back(start+"->"+end);
+        }
+        return result;
+    }
+};
