dabbbe7bd6f0f6a2982702320f65b0bca241695a
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-03-01 00:26:36 +0530

    Time: 547 ms (5.14%), Space: 37.7 MB (75.10%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+from queue import Queue
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isEvenOddTree(self, root: TreeNode) -> bool:
+        if root is None:
+            return False
+        else:
+            q = Queue()
+            q.put(root)
+            currentLevelCount = 1
+            current: bool = False
+            while q.qsize() != 0:
+                nextLevelCount = 0 
+                currentLevelVal = 0
+                lastSeenValue = None
+                while currentLevelVal != currentLevelCount:
+                    currentNode: TreeNode = q.get()
+                    if currentNode.left:
+                        q.put(currentNode.left)
+                        nextLevelCount += 1
+                    if currentNode.right:
+                        q.put(currentNode.right)
+                        nextLevelCount += 1
+                    # print(currentNode.val, currentLevelVal, nextLevelCount, q.qsize(), lastSeenValue, currentLevelCount)
+                    if currentLevelVal == 0:
+                        if  not current:  # value should be odd and increasing left to right
+                            if currentNode.val % 2 == 0:
+                                return False
+                        else:  # val should be even and decreasing left to right
+                            if currentNode.val % 2 != 0:
+                                return False
+                        lastSeenValue = currentNode.val
+                    else:
+                        if not current:
+                            if currentNode.val % 2 == 0 or lastSeenValue >= currentNode.val:
+                                return False
+                        else:
+                            if currentNode.val % 2 != 0 or lastSeenValue <= currentNode.val:
+                                return False
+                        lastSeenValue = currentNode.val
+                    currentLevelVal += 1
+                currentLevelCount = nextLevelCount
+                current = not current
+            return True
