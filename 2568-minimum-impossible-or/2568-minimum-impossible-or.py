0d318942daea6f2a1819d2c4ffdf7ad9a859196d
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-03-06 22:05:32 +0530

    Time: 363 ms (22.99%), Space: 40.2 MB (8.05%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.py
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.py
@@ -2,14 +2,13 @@ class Solution:
     def minImpossibleOR(self, nums: List[int]) -> int:
         # Find the lowest power of 2 and use it if [1, 2] in the list then [1, 2^3 +1 ] range of numbers can be formed
         setNums = set(nums)
+        itemDict = {}
+        for num in setNums:
+            itemDict[num] = True
         solution = 0 
         for i in range(32):
             current = 2** i 
-            currentInNums=False
-            for num in setNums:
-                if num == current:
-                    currentInNums = True
-            if currentInNums:
+            if itemDict.get(current) is not None:
                 solution+=current
             else:
                 return solution+1
