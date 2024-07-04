371bd45f6e22cd0a0fe47925ba597ac632ca6657
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-07-04 15:33:59 +0530

    Time: 80 ms (5.33%), Space: 16.6 MB (80.12%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,48 @@
+class Solution:
+    wordDict:dict
+    dp:dict
+        
+    # @lru_cache(maxsize=None)
+    def rec(self, word, startIndex, endIndex):
+        """
+        """
+        if endIndex>len(word):
+            return False # base case 1
+        currentWord = word[startIndex:endIndex]
+        if self.wordDict.get(currentWord) is not None:
+            # Winning match
+            # print(currentWord)
+            if endIndex == len(word):
+                return True
+            acceptSol = False
+            rejectSol = False
+            if self.dp.get(f"{startIndex}-{endIndex+1}") is not None:
+                acceptSol = self.dp.get(f"{startIndex}-{endIndex+1}") #accept the word
+                
+            else:
+                acceptSol = self.rec(word, startIndex, endIndex+1) #accept the word
+                
+            if self.dp.get(f"{endIndex}-{endIndex+1}") is not None:
+                rejectSol = self.dp.get(f"{endIndex}-{endIndex+1}") #accept the word
+                
+            else:
+                rejectSol = self.rec(word, endIndex, endIndex+1) # disregard the word
+            self.dp[f"{startIndex}-{endIndex}"] = acceptSol or rejectSol
+            return acceptSol or rejectSol
+        else:
+            return self.rec(word, startIndex, endIndex+1) # No match we move forward
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """
+            The idea is to start from the beginning, call rec function from beginning
+            winning case is reaching end of the string and it being a match
+            base case is surpassing the end of the string also a losing case
+            on every match, we need to call rec two time, once-> Two accept the current solution/ Once to move ahead without accepting the current solution 
+            Try using lruCache?
+        """
+        self.wordDict = {}
+        self.dp = {}
+        for word in wordDict:
+            self.wordDict[word] = True
+        return self.rec(s, 0, 1)
+        
+        
