5471e92858bb7b25fa2d49f4e4c9bc826685688a
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-07-09 15:12:37 +0530

    Time: 521 ms (16.46%), Space: 29.5 MB (31.01%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        """
+        It is sort of like two sum
+        """
+        occurDict = defaultdict(int)
+        for num in nums:
+            occurDict[num]+=1
+        res=0
+        for num in nums:
+            if num<k and k-num==num:
+                if occurDict.get(num)>1:
+                    res+=1
+                    occurDict[num]-=2
+            else:
+                if num<k and occurDict.get(k-num) is not None and occurDict[k-num]>0 and occurDict[num]>0:
+                    
+                    res+=1
+                    occurDict[num]-=1
+                    occurDict[k-num]-=1
+        return res
+        
