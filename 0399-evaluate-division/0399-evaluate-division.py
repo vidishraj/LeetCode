0738a6f02f3ffedeed07a7e0be99ed4de8ec88b9
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-07-15 16:11:42 +0530

    Time: 58 ms (5.66%), Space: 17.2 MB (11.74%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,29 +1,55 @@
-from collections import defaultdict, deque
-from typing import List, Dict
+import queue
 
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def build_graph(equations: List[List[str]], values: List[float]) -> Dict[str, Dict[str, float]]:
-            graph = defaultdict(dict)
-            for (num, denom), value in zip(equations, values):
-                graph[num][denom] = value
-                graph[denom][num] = 1 / value
-            return graph
-        
-        def bfs(start: str, end: str) -> float:
-            if start not in graph or end not in graph:
-                return -1.0
-            q = deque([(start, 1.0)])
-            visited = set()
-            while q:
-                current, current_value = q.popleft()
-                if current == end:
-                    return current_value
-                visited.add(current)
-                for neighbor, value in graph[current].items():
-                    if neighbor not in visited:
-                        q.append((neighbor, current_value * value))
-            return -1.0
-        
-        graph = build_graph(equations, values)
-        return [bfs(num, denom) for num, denom in queries]
+        """
+        Will run bfs to remake the graph
+        """
+        graph={}
+        for index,eq in enumerate(equations):
+            if graph.get(eq[0]) is None:
+                graph[eq[0]] = {eq[1]:values[index]}
+                if graph.get(eq[1]) is None:
+                    graph[eq[1]] = {eq[0]:1/values[index]}
+                else:
+                    graph[eq[1]][eq[0]]=1/values[index]
+            else:
+                graph[eq[0]][eq[1]] = values[index]
+                if graph.get(eq[1]) is None:
+                    graph[eq[1]] = {eq[0]:1/values[index]}
+                else:
+                    graph[eq[1]][eq[0]]=1/values[index]
+        q = queue.Queue()
+        keys= list(graph.keys())
+        for key in keys:
+            for denom in graph[key]:
+                q.put([denom, graph[key][denom]])
+            visited = {}
+            while q.qsize()>0:
+                curr = q.get()
+                currVal = curr[1]
+                visited[curr[0]] = True
+                if curr[0] in graph:
+                    for val in graph[curr[0]]:
+                        if visited.get(val) is None:
+                            q.put([val, graph[curr[0]][val]*currVal])
+                        if graph[key].get(val) is None:
+                            graph[key][val] = graph[curr[0]][val]*currVal
+
+        res = []  
+        print(graph)
+        for query in queries:
+            denom = query[1]
+            numer = query[0]
+            if numer == denom:
+                if graph.get(denom) is None and graph.get(numer) is None:
+                    res.append(-1.00000)
+                else:
+                    res.append(1.00000)
+            elif graph.get(numer) is not None and graph.get(numer).get(denom) is not None:
+                res.append(graph.get(numer).get(denom))
+            elif graph.get(denom)is not None and graph.get(denom).get(numer) is not None:
+                res.append(1/graph.get(denom).get(numer))
+            else:
+                res.append(-1.0000)
+        return res
