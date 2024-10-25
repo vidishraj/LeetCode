e446e0608ced468091067761060064181b0d59f4
Vidish Raj <66262548+vidishraj@users.noreply.github.com>
2024-10-25 08:39:13 +0530

    Time: 31 ms (93.37%), Space: 19.6 MB (14.41%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,21 +1,23 @@
+from copy import deepcopy
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         """
-        At each point in the matrix, we need to check. We need to move backwards from the bottom of the matrix. 
-        Essentially, we move in the opposite direction and fill our dp matrix
+        The idea isnt to iterate from the top to the bottom but rather from the bottom to the top and 
+        check the choices we are presented
         """
-        dp = []
-        for i in range(len(grid)):
-            dp.append([])
-            for j in range(len(grid[0])):
-                dp[i].append(0)
-        dp[0][0] = grid[0][0]
-        for i in range(1,len(grid)):
-            dp[i][0] += grid[i][0]+dp[i-1][0]
-        for i in range(1, len(grid[0])):
-            dp[0][i] += grid[0][i]+dp[0][i-1]
-        for i in range(1, len(grid)):
-            for j in range(1, len(grid[0])):
-                dp[i][j] = min(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j])
-        return dp[-1][-1]
+        dp = deepcopy(grid)
+        rows = len(grid)
+        cols = len(grid[0])
+        for i in range(rows-2, -1, -1):
+          dp[i][cols-1] +=dp[i+1][cols-1]
+        for i in range(cols-2, -1, -1):
+          dp[rows-1][i] +=dp[rows-1][i+1]
+        for i in range(rows-2, -1, -1):
+            for j in range(cols-2, -1, -1):
+                rightSum = dp[i+1][j] + grid[i][j]
+                downSum = dp[i][j+1] + grid[i][j]
+                dp[i][j] = min(rightSum, downSum)
+        # print(dp)
+        return dp[0][0]
+        
             
